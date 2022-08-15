c5c174b69639fbdef477630ca37558a2c891a4da
Ren Hiyama <renhiyama@rovelstars.com>
2022-08-15 11:38:35 +0100

    brew

---
+++ breew.sh
@@ -0,0 +1,4 @@
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+ echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> /home/user/.zshrc
+    eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+brew install thefuck
